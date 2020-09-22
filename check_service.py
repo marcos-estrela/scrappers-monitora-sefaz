d113942987a8cb12d0e12f08c3a22179aa16adc7
marcos-estrela <marcos.estrela@hotmail.com>
2020-09-21 22:18:11 -0300

    Adiciona primeira estrutura útil do scrapper

--- check_service.py
+++ check_service.py
@@ -1,11 +1,20 @@
+# TODO: Validador de estrutura do site (Enviar alguma mensagem caso tenha problema)
+# TODO: Separação de estrutura entre os estados
+# TODO: Salvar estrutura com horário
+
 import scrapy
+# import os
+import pymongo
+from decouple import config
+
+statusAtual = {}
 
 
 class ScraperSefaz(scrapy.Spider):
     name = 'scrape-table'
-    allowed_domains = ['https://www.nfe.fazenda.gov.br/portal/disponibilidade.aspx']
+    allowed_domains = [
+        'https://www.nfe.fazenda.gov.br/portal/disponibilidade.aspx']
     start_urls = ['https://www.nfe.fazenda.gov.br/portal/disponibilidade.aspx']
-
 
     def start_requests(self):
         urls = [
@@ -15,19 +24,29 @@ class ScraperSefaz(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        api = config('MONGODB_URI')
+        client = pymongo.MongoClient(api)
+        db = client['monitora-sefaz']
+        collection = db['current_status']
+        # objStatus = db.current_status
+
         for linha in response.xpath('//*[@class="linhaImparCentralizada" or @class="linhaParCentralizada"]'):
             estado = linha.xpath('td//text()')[0].extract()
 
             autorizacao = self.verificaStatus(linha.xpath('td')[1].extract())
-            retorno_autorizacao = self.verificaStatus(linha.xpath('td')[2].extract())
+            retorno_autorizacao = self.verificaStatus(
+                linha.xpath('td')[2].extract())
             inutilizacao = self.verificaStatus(linha.xpath('td')[3].extract())
-            consulta_protocolo = self.verificaStatus(linha.xpath('td')[4].extract())
-            status_servico = self.verificaStatus(linha.xpath('td')[5].extract())
-            consulta_cadastro = self.verificaStatus(linha.xpath('td')[7].extract())
-            recepcao_evento = self.verificaStatus(linha.xpath('td')[8].extract())
+            consulta_protocolo = self.verificaStatus(
+                linha.xpath('td')[4].extract())
+            status_servico = self.verificaStatus(
+                linha.xpath('td')[5].extract())
+            consulta_cadastro = self.verificaStatus(
+                linha.xpath('td')[7].extract())
+            recepcao_evento = self.verificaStatus(
+                linha.xpath('td')[8].extract())
 
-            yield {
-        		'estado': estado,
+            statusAtual[estado] = {
                 'autorizacao': autorizacao,
                 'retorno_autorizacao': retorno_autorizacao,
                 'inutilizacao': inutilizacao,
@@ -36,6 +55,7 @@ class ScraperSefaz(scrapy.Spider):
                 'consulta_cadastro': consulta_cadastro,
                 'recepcao_evento': recepcao_evento
             }
+        collection.insert_one(statusAtual)
 
     def verificaStatus(self, coluna):
         status = 'Indefinido'
